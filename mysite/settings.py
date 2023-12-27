31f95b5ecf5ba1ddd4bd7c1f05cd74a0e5a35a6e
octocat9lee <octocat9lee@gmail.com>
2023-12-27 11:40:19 +0800

    model（https://www.liujiangblog.com/course/django/88）

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',  # 'polls'应用
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
